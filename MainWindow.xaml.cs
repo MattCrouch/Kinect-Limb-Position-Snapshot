46828b89e9b8e23bf87655b17e2a20e0745071ac
Matt Crouch <matt@mattcrouch.net>
2012-04-13 17:32:49 +0100

    Double clicking values in textboxes now copies that value to the clipboard

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,9 +7,6 @@ using Microsoft.Kinect;
 
 namespace LimbPositionSnapshot
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
